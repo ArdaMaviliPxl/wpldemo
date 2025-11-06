bf9b08a13328261fcb7e9cdef429d082348def8b
Arda Mavili <Arda.Mavili@PXL.STUDENT.BE>
2025-11-06 13:18:03 +0100

    DEMO-01

---
+++ wpldemo/wpldemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace wpldemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gberuikersnaam: ");
+            string gebruikersnaam = Console.ReadLine();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();  
+        }
+    }
+}
